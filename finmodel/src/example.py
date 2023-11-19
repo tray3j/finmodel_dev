61d93d61a80b20af348984f6a25595fb616b4966
Trey Jackson <jackson.trey@protonmail.com>
2023-11-19 15:47:19 -0600

    updates to example

--- finmodel/src/example.py
+++ finmodel/src/example.py
@@ -11,17 +11,33 @@ gadgets = pd.Series(data=[60,70,85,100,125],index=actl_periods,name='gadgets')
 gizmos = pd.Series(data=[200,210,217,228,239],index=actl_periods,name='gizmos')
 # Enchancements: 1) have these items populate some global 'account' dict for accessing / consolidating
 # 2) functionally generate instead of hardcoding (of course)
-accounts = pd.concat([gadgets, gizmos],axis=0,keys=[gadgets.name, gizmos.name],names=['Account','Year'])
-print(accounts.info())
+actuals = pd.concat([gadgets, gizmos],axis=0,keys=[gadgets.name, gizmos.name],names=['Account','Year'])
+print(actuals.info())
 # instantiate metrics dict which holds the logic for defining metrics
+
+def level_mix_calc(df, levels): # conveniently calculate percentage mix
+    '''calculates the percentage mix of a particular value in an index level of a df. 
+    Returns a df'''
+    base = df.groupby(level=levels).transform(lambda x: x.sum())
+    mix = df / base
+    return mix
+
 metrics_dict = {
     'YoY Growth' : {
         'func': lambda x: x.pct_change(),
-        'name': 'yoy_growth'
+        'name': 'yoy_growth',
+        'other_args' : None,
+        'group_levels' : ['Account']
+    },
+    'Gizmo mix' : {
+        'func': level_mix_calc,
+        'other_args' : dict(levels=['Year']),
+        'name': 'gizmo_mix',
+        'group_levels' : None
     }
 }
 
-def calc_metrics(metrics_dict, actuals, group_by_levels=None):
+def calc_metrics(metrics_dict, actuals):#, group_by_levels=None):
     '''Calculates metrics on provided actuals.
     Returns a list of metrics.
     parameters:
@@ -33,19 +49,22 @@ def calc_metrics(metrics_dict, actuals, group_by_levels=None):
         group_by_levels=None : for actuals of dtype dataframe or series with a multi-index that needs to be grouped before passing 
         into the metrics func
     '''
-    metrics = []
+    metrics = pd.DataFrame(index=actuals.index)
     for metric in metrics_dict.values():
-        if group_by_levels:
-            grouped = actuals.groupby(level=group_by_levels,group_keys=False)
-            metrics = grouped.apply(metric['func'])
-            metrics.name = metric['name']
+        if metric['group_levels']:
+            grouped = actuals.groupby(level=metric['group_levels'],group_keys=False)
+            if metric['other_args']:
+                metrics[metric['name']] = metric['func'](grouped,**metric['other_args'])
+            else:
+                metrics[metric['name']] = metric['func'](grouped)
+            #metrics.name = metric['name']
             #metrics = metrics.add_suffix(metric.get('name'))    
         else:
-            for actual in actuals:
-                metric_name = actual.name + metric.get('name')
-                metric_output = metric['func'](actual)
-                metric_output.name = metric_name
-                metrics.append(metric_output)    
+            print('due to no group_levels being passed in metric, this operation is not yet supported')    
+            if metric['other_args']:
+                metrics[metric['name']] = metric['func'](actuals,**metric['other_args'])
+            else:
+                metrics[metric['name']] = metric['func'](actuals)
     
     if metrics.index.equals(actuals.index):
         print('Indexes are aligned')
@@ -53,8 +72,8 @@ def calc_metrics(metrics_dict, actuals, group_by_levels=None):
         print('WARNING: Index of metrics is not equal to index of actuals. Alignment and calculations may not work as expected')
     return metrics
 
-growth = calc_metrics(metrics_dict=metrics_dict, actuals=accounts,group_by_levels=['Account'])
+growth = calc_metrics(metrics_dict=metrics_dict, actuals=actuals)
 
-print(accounts)
+print(actuals)
 print(growth)
 
