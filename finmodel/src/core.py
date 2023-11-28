0d200e13f3a3ee636d4101495f684404f75f6343
Trey Jackson <jackson.trey@protonmail.com>
2023-11-28 08:09:10 -0600

    worked on dev readme and core classes. lots to do

--- finmodel/src/core.py
+++ finmodel/src/core.py
@@ -2,17 +2,43 @@ import pandas as pd
 import numpy as np
 import render
 
-class Account (pd.DataFrame):
+
+
+
+
+class Actuals(pd.DataFrame):
+    '''Class for holding a actuals data with related features. 
+    Inherits pd.DataFrame. minimally requires an index and accounts.
+    Implies the size and shape of data needed for model.
+    Data is loaded with the .load() method ''' 
     
-    def init(self,name,data,index):
-        self.name = name
+    def init(self,accounts,index):
         self.index = index
-        self.data = data
+        self.accounts = accounts
+        self.data = None
+
+    def load(self,df):
+        self.accounts = df.columns
+        # TODO should accounts be instantiated here or in the init method?
+        if (df.index == self.index and self.data == None): # checks if actuals is empty
+            self = df
+        else:
+            self.loc[df.index,df.columns] = df
+
+
+class Metrics(pd.DataFrame):
+    '''Class for holding metric calculations and results'''
+
+    def init(self, metrics_dict, actuals):
+        self.columns = metrics_dict.keys()
+        
+
 
 class Model:
-
-    def init(self, name, accounts):
+# TODO Have barely started this
+    def init(self, name, actuals):
         self.name = name
-        self.Accounts = accounts
+        self.Accounts = actuals.accounts
+
 
 
