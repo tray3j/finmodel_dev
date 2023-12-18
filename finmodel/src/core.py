2a6ad08b061b2cf78e87f93dd9f294636e6176ba
Trey Jackson <jackson.trey@protonmail.com>
2023-12-18 13:46:58 -0600

    Refactoring some things

--- finmodel/src/core.py
+++ finmodel/src/core.py
@@ -3,42 +3,70 @@ import numpy as np
 import render
 
 
-
-
-
-class Actuals(pd.DataFrame):
-    '''Class for holding a actuals data with related features. 
-    Inherits pd.DataFrame. minimally requires an index and accounts.
-    Implies the size and shape of data needed for model.
-    Data is loaded with the .load() method ''' 
-    
-    def init(self,accounts,index):
-        self.index = index
-        self.accounts = accounts
-        self.data = None
-
-    def load(self,df):
-        self.accounts = df.columns
-        # TODO should accounts be instantiated here or in the init method?
-        if (df.index == self.index and self.data == None): # checks if actuals is empty
-            self = df
+def calc_metrics(metrics_dict, actuals):#, group_by_levels=None):
+    '''Calculates metrics on provided actuals.
+    Returns a list of metrics.
+    parameters:
+        metrics_dict: dict with metrics, expects each 'metric' to have
+        keys of 'name' and 'func' with name of the metric to be appended onto end of each actual
+        and 'func' which should take one argument of the actual being passed and return an identically shaped and indexed array or scalar
+        actuals: a list of pd.series-like objects appropriately indexed. If func in metrics dict calls object methods on actuals, actuals must
+        have that method be callable # TODO what does appropriately indexed mean??
+        group_by_levels=None : for actuals of dtype dataframe or series with a multi-index that needs to be grouped before passing 
+        into the metrics func
+    '''
+    metrics = pd.DataFrame(index=actuals.index)
+    for metric in metrics_dict.values():
+        if metric['group_levels']:
+            grouped = actuals.groupby(level=metric['group_levels'],group_keys=False)
+            if metric['other_args']:
+                metrics[metric['name']] = metric['func'](grouped,**metric['other_args'])
+            else:
+                metrics[metric['name']] = metric['func'](grouped)
+            #metrics.name = metric['name']
+            #metrics = metrics.add_suffix(metric.get('name'))    
         else:
-            self.loc[df.index,df.columns] = df
+            if metric['other_args']:
+                metrics[metric['name']] = metric['func'](actuals,**metric['other_args'])
+            else:
+                metrics[metric['name']] = metric['func'](actuals)
+    
+    if metrics.index.equals(actuals.index):
+        print('Indexes are aligned')
+    else:
+        print('WARNING: Index of metrics is not equal to index of actuals. Alignment and calculations may not work as expected')
+    return metrics
 
+def simple_growth(input,g,nper):
+    '''grow input exponentially at growth rate g for nper number of periods'''
+    exps = np.arange(start=1,stop=nper+1,step=1)
+    factors = np.cumprod((1+g),axis=0)
+    result = input*factors
+    return result
 
-class Metrics(pd.DataFrame):
-    '''Class for holding metric calculations and results'''
+def level_mix_calc(df, levels): # conveniently calculate percentage mix
+    '''calculates the percentage mix of a particular value in an index level of a df. 
+    Returns a df'''
+    base = df.groupby(level=levels).transform(lambda x: x.sum())
+    mix = df / base
+    return mix
 
-    def init(self, metrics_dict, actuals):
-        self.columns = metrics_dict.keys()
-        
+def model_calc(model_dict, fcst_index):
+    ''' takes in model dict. Returns a dataframe with index fcst_index,
+    using models (function calls defined in model dict) to generate similarly indexed df'''
+    placeholder = pd.DataFrame(index=fcst_index)
+    model = pd.DataFrame() # doing it this way so that the model is only indexed insofar as there are model elements with that index. 
+    # drawback is that we may end up with a model differently indexed from our actuals
+    for account,values in model_dict.items():
+        print(f'modeling account: {account} with value {values["model"]}')
+        # build the components of model df bit by bit
+        result_array = values['model']
+        print(result_array)
+        result_index = placeholder.xs(account,0,level='Account',drop_level=False).index
+        result = pd.DataFrame(data=result_array,index=result_index)
+        model = pd.concat([model,result],axis=0)
+    return model
 
-
-class Model:
-# TODO Have barely started this
-    def init(self, name, actuals):
-        self.name = name
-        self.Accounts = actuals.accounts
 
 
 
--- finmodel/src/example.py
+++ finmodel/src/example.py
@@ -2,6 +2,7 @@
 # A minimal, simplistic example
 import pandas as pd
 import numpy as np
+import core as cr
 from render import render_df
 
 actl_periods = pd.PeriodIndex(pd.period_range(2019,2023,freq='A-DEC'))
@@ -18,47 +19,6 @@ actuals.name='Sales'
 
 # instantiate metrics dict which holds the logic for defining metrics
 
-def calc_metrics(metrics_dict, actuals):#, group_by_levels=None):
-    '''Calculates metrics on provided actuals.
-    Returns a list of metrics.
-    parameters:
-        metrics_dict: dict with metrics, expects each 'metric' to have
-        keys of 'name' and 'func' with name of the metric to be appended onto end of each actual
-        and 'func' which should take one argument of the actual being passed and return an identically shaped and indexed array or scalar
-        actuals: a list of pd.series-like objects appropriately indexed. If func in metrics dict calls object methods on actuals, actuals must
-        have that method be callable # TODO what does appropriately indexed mean??
-        group_by_levels=None : for actuals of dtype dataframe or series with a multi-index that needs to be grouped before passing 
-        into the metrics func
-    '''
-    metrics = pd.DataFrame(index=actuals.index)
-    for metric in metrics_dict.values():
-        if metric['group_levels']:
-            grouped = actuals.groupby(level=metric['group_levels'],group_keys=False)
-            if metric['other_args']:
-                metrics[metric['name']] = metric['func'](grouped,**metric['other_args'])
-            else:
-                metrics[metric['name']] = metric['func'](grouped)
-            #metrics.name = metric['name']
-            #metrics = metrics.add_suffix(metric.get('name'))    
-        else:
-            if metric['other_args']:
-                metrics[metric['name']] = metric['func'](actuals,**metric['other_args'])
-            else:
-                metrics[metric['name']] = metric['func'](actuals)
-    
-    if metrics.index.equals(actuals.index):
-        print('Indexes are aligned')
-    else:
-        print('WARNING: Index of metrics is not equal to index of actuals. Alignment and calculations may not work as expected')
-    return metrics
-
-def level_mix_calc(df, levels): # conveniently calculate percentage mix
-    '''calculates the percentage mix of a particular value in an index level of a df. 
-    Returns a df'''
-    base = df.groupby(level=levels).transform(lambda x: x.sum())
-    mix = df / base
-    return mix
-
 metrics_dict = {
     'YoY Growth' : {
         'func': lambda x: x.pct_change(),
@@ -67,67 +27,43 @@ metrics_dict = {
         'group_levels' : ['Account']
     },
     'Gizmo mix' : {
-        'func': level_mix_calc,
+        'func': cr.level_mix_calc,
         'other_args' : dict(levels=['Year']),
         'name': 'gizmo_mix',
         'group_levels' : None
     }
 }
 
-def simple_growth(input,g,nper):
-    '''grow input exponentially at growth rate g for nper number of periods'''
-    exps = np.arange(start=1,stop=nper+1,step=1)
-    factors = np.cumprod((1+g),axis=0)
-    result = input*factors
-    return result
 
-
-growth = calc_metrics(metrics_dict=metrics_dict, actuals=actuals)
+growth = cr.calc_metrics(metrics_dict=metrics_dict, actuals=actuals)
+# growth here being the name of our metrics df
 
 assumption_dict = {
     'gadget_growth': {
-        'growth_func' : simple_growth, # TODO: remove, belongs in model step
+        'growth_func' : cr.simple_growth, # TODO: remove, belongs in model step
         'growth' : np.full(5,growth.xs(key=('gadgets'))['yoy_growth'].mean())
-    },
-
-    'gizmo_growth': {
-        'competition' : pd.Series([False, False, True, True, True],index=fcst_periods),
-        'growth_func' : simple_growth, # TODO: remove, belongs in model step
-        'no_competition_growth' : growth.query("Account == 'gizmos'").iloc[-2:]['yoy_growth'].mean(),
-        'with_competition_growth' : "assumption_dict['gadget_growth']['growth']", # NEEDS REPLACEMENT AFTER THE FACT
-        'rationale' : """Assumes the growth observed in the last two actual periods continues until competition enters. Then,
-        grow at the same rate as gadgets have in the last two actual periods"""
+    }
+} # split up because of inter-assumption dependencies #TODO better way to handle these dependcies?
+assumption_dict['gizmo_growth'] = {
+    'competition' : pd.Series([False, False, True, True, True],index=fcst_periods),
+    'growth_func' : cr.simple_growth, # TODO: remove, belongs in model step
+    'no_competition_growth' : growth.query("Account == 'gizmos'").iloc[-2:]['yoy_growth'].mean(),
+    'with_competition_growth' : assumption_dict['gadget_growth']['growth'],
+    'rationale' : """Assumes the growth observed in the last two actual periods continues until competition enters. Then,
+    grow at the same rate as gadgets have in the last two actual periods"""
     }
     
-}
-assumption_dict['gizmo_growth']['with_competition_growth'] = eval(assumption_dict['gizmo_growth']['with_competition_growth']) # is this legal?
-
 model_dict = { # is this more appropriate as a function somehow?
     'gadgets': {
-        'model' : simple_growth(input=actuals.xs('gadgets').iloc[-1],g=assumption_dict['gadget_growth']['growth'],nper=len(fcst_periods))
+        'model' : cr.simple_growth(input=actuals.xs('gadgets').iloc[-1],g=assumption_dict['gadget_growth']['growth'],nper=len(fcst_periods))
         },
     'gizmos': {
-        'model' : simple_growth(input=actuals.xs('gizmos').iloc[-1],g=np.where(assumption_dict['gizmo_growth']['competition'],assumption_dict['gizmo_growth']['no_competition_growth'],assumption_dict['gizmo_growth']['with_competition_growth']),nper=len(fcst_periods))
+        'model' : cr.simple_growth(input=actuals.xs('gizmos').iloc[-1],g=np.where(assumption_dict['gizmo_growth']['competition'],assumption_dict['gizmo_growth']['no_competition_growth'],assumption_dict['gizmo_growth']['with_competition_growth']),nper=len(fcst_periods))
         }
 }
 
-def model_calc(model_dict, fcst_index):
-    ''' takes in model dict. Returns a dataframe with index fcst_index,
-    using models (function calls defined in model dict) to generate similarly indexed df'''
-    placeholder = pd.DataFrame(index=fcst_index)
-    model = pd.DataFrame()
-    for account,values in model_dict.items():
-        print(f'modeling account: {account} with value {values["model"]}')
-        # build the components of model df bit by bit
-        result_array = values['model']
-        print(result_array)
-        result_index = placeholder.xs(account,0,level='Account',drop_level=False).index
-        result = pd.DataFrame(data=result_array,index=result_index)
-        model = pd.concat([model,result],axis=0)
-    return model
-
 model_index = pd.MultiIndex.from_product([actuals.index.get_level_values(0).unique().to_list(),fcst_periods],names=['Account','Year'])
-model = model_calc(model_dict=model_dict, fcst_index=model_index)
+model = cr.model_calc(model_dict=model_dict, fcst_index=model_index)
 model.columns = ['Sales'] # TODO make this not hardcoded
 
 # We should wrap up these last few steps in a function
@@ -135,12 +71,8 @@ output = pd.concat([actuals.to_frame(),model],axis=0,keys=['Actual','Forecast'],
 
 full_metrics_dict = metrics_dict
 full_metrics_dict['YoY Growth']['group_levels'] = ['Account']
-output_metrics = calc_metrics(metrics_dict=full_metrics_dict,actuals=output)
+output_metrics = cr.calc_metrics(metrics_dict=full_metrics_dict,actuals=output)
 output = pd.concat([output,output_metrics],axis=1)
 
-
-
-# print(actuals)
-# print(growth)'
 render_df(output)
 
